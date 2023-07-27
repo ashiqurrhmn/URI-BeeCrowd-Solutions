36e19532df1c05edbb28ce91bebae32b520415c0
Md. Ashiqur Rahman <138354564+ashiqurrhmn@users.noreply.github.com>
2023-07-27 22:08:59 +0600

    Add files via upload

---
+++ 1078.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+    cin>>n;
+    i=1;
+    while(i<11)
+    {
+        cout<<i<<" x "<<n<<" = "<<i*n<<endl;
+        i++;
+    }
+
+    return 0;
+}
