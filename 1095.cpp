bb2816b228aa7f9598fd78d046f318dc09361119
Md. Ashiqur Rahman <138354564+ashiqurrhmn@users.noreply.github.com>
2023-07-28 23:19:28 +0600

    Add files via upload

---
+++ 1095.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int j, i;
+    for (i=1, j=60; i<=37, j>=0; i+=3, j-=5)
+    {
+        cout<<"I="<<i<<" "<<"J="<<j<<endl;
+    }
+
+    return 0;
+}
---
+++ 1095.py
@@ -0,0 +1,5 @@
+j = 60
+for i in range(1, 38, 3):
+    print(f"I={i} J={j}")
+    i = i + 3
+    j = j - 5
